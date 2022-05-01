7fa955b3737115c8572654126df003a2117d3971
Lucas <lucasgunner3@gmail.com>
2022-05-01 12:09:41 -0300

    Back in black

---
+++ src/ApiNetCoreDDD.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ApiNetCoreDDD.Domain.Entities.Base
+{
+    public abstract class Entity
+    {
+        protected Entity(DateTime? createdAt, 
+            DateTime? updatedAt)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = createdAt ?? DateTime.UtcNow;
+            UpdatedAt = updatedAt;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime? CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+    }
+}
---
+++ src/ApiNetCoreDDD.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using ApiNetCoreDDD.Domain.Entities.Base;
+using System;
+
+namespace ApiNetCoreDDD.Domain.Entities
+{
+    public class User : Entity
+    {
+        public User(DateTime? createdAt, DateTime? updatedAt, string nome, string email)
+            : base(createdAt, updatedAt)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+    }
+}

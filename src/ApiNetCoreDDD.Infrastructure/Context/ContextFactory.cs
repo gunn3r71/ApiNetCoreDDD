4830b6fe19d697bee9b524a6694bd5feba3bdd55
Lucas <lucasgunner3@gmail.com>
2022-05-01 13:02:20 -0300

    Factory documentation

--- src/ApiNetCoreDDD.Infrastructure/Context/ContextFactory.cs
+++ src/ApiNetCoreDDD.Infrastructure/Context/ContextFactory.cs
@@ -6,11 +6,14 @@ namespace ApiNetCoreDDD.Infrastructure.Context
 {
     public class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
+        /// <summary>
+        /// Create a context for design time (Migrations)
+        /// </summary>
         public ApplicationDbContext CreateDbContext(string[] args)
         {
             var connectionString = "Server=localhost;Port=3306;Database=dddAPI;Uid=nse;Pwd=development@lucas1A;";
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            
+
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), options => {
                 options.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
                 options.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
